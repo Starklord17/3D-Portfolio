0d897cf954c30e3e4ce8f89c20f0e16bbef42480
MaxSM <maxi.sm117@gmail.com>
2023-03-14 18:55:11 -0300

    :zap: Navbar, Hero, postcss.config

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,8 +19,8 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Max</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            Full Stack JavaScript Developer <br className='sm:block hidden' />
+            & Blockchain Developer
           </p>
         </div>
       </div>
