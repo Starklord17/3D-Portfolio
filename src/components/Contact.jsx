076f9fb5eb58cdec3fd929a3e2211033c6abe148
MaxSM <maxi.sm117@gmail.com>
2023-10-07 10:14:31 -0300

    :zap: EmailJS

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,10 +33,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        // import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        // import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-        'service_prgdgbq',
-        'template_p6fqu1y',
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
           to_name: "Max",
@@ -44,8 +42,7 @@ const Contact = () => {
           to_email: "maxi.sm117@gmail.com",
           message: form.message,
         },
-        // import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-        'yWu_pWs-FSmhEUP0r'
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
